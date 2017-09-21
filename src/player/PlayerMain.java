907ae846808c17846bca9d9c6956f6c04160563e
Pasi Junttila <pasi.junttila@live.com>
2017-09-21 12:44:03 +0300

    List of stations to PlayerMain, some static choices.

--- src/player/PlayerMain.java
+++ src/player/PlayerMain.java
@@ -3,16 +3,31 @@ package player;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import player.model.RadioStation;
 
 public class PlayerMain extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
+    private ObservableList<RadioStation> stationList = FXCollections.observableArrayList();
+
+    public PlayerMain() {
+        stationList.add(new RadioStation("80splanet.com", "http://23.92.61.227:9020"));
+        stationList.add(new RadioStation("ROCK ANTENNE Heavy Metal", "http://mp3channels.webradio.antenne.de:80/heavy-metal"));
+        stationList.add(new RadioStation("RADIO ENERGY (NRJ), 90s", "http://stream.radioreklama.bg:80/nrj128"));
+        stationList.add(new RadioStation("Breakz.FM - DJ Radio", "http://radio2.breakz.fm:8000/stream128.mp3"));
+    }
+
+    public ObservableList<RadioStation> getStationList() {
+        return stationList;
+    }
 
     public Stage getPrimaryStage() {
         return this.primaryStage;
@@ -38,7 +53,6 @@ public class PlayerMain extends Application {
         catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     private void showPlayerMain() {
