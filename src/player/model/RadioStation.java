cf37c8741ef5414c409a4c157616abb2b407d545
Pasi Junttila <pasi.junttila@live.com>
2017-09-21 12:36:00 +0300

    Model for radio station.

---
+++ src/player/model/RadioStation.java
@@ -0,0 +1,44 @@
+package player.model;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class RadioStation {
+
+    private StringProperty name;
+    private StringProperty stationUrl;
+
+    public RadioStation() {
+    }
+
+    public RadioStation(String name, String stationUrl) {
+        super();
+        this.name = new SimpleStringProperty(name);
+        this.stationUrl = new SimpleStringProperty(stationUrl);
+    }
+
+    public StringProperty getNameProperty() {
+        return name;
+    }
+
+    public String getName() {
+        return this.name.get();
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public StringProperty getStationUrlProperty() {
+        return stationUrl;
+    }
+
+    public String getStationUrl() {
+        return this.stationUrl.get();
+    }
+
+    public void setStationUrl(String stationUrl) {
+        this.stationUrl.set(stationUrl);
+    }
+
+}
