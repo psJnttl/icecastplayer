00aafee2edec0fc13bd37b8a453e6ef3c2a93bba
Pasi Junttila <pasi.junttila@live.com>
2017-09-21 12:30:02 +0300

    Main view: all components visible.

--- src/player/PlayerMain.java
+++ src/player/PlayerMain.java
@@ -1,16 +1,60 @@
 package player;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class PlayerMain extends Application {
 
-	@Override
-	public void start(Stage primaryStage) {
-		
-	}
+    private Stage primaryStage;
+    private BorderPane rootLayout;
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public Stage getPrimaryStage() {
+        return this.primaryStage;
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("NetRadioPlayer");
+        itializeRootLayout();
+        showPlayerMain();
+    }
+
+    private void itializeRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(PlayerMain.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void showPlayerMain() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(PlayerMain.class.getResource("view/PlayerMain.fxml"));
+            AnchorPane playerMainView = (AnchorPane) loader.load();
+            rootLayout.setCenter(playerMainView);
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
